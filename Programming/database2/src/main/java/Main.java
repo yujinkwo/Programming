ed704ae8e5940de14fd4c6318ae41752cc4b8690
yujinkwo <2369634@learn.vsb.bc.ca>
2022-06-01 14:51:30 -0700

    database

--- Programming/database2/src/main/java/Main.java
+++ Programming/database2/src/main/java/Main.java
@@ -1,20 +1,78 @@
 import database.DatabaseHandler;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Scanner;
+
 
 public class Main {
-    public static void main(String[] args) {
-        DatabaseHandler handler=new DatabaseHandler();
-        String folderPath = "C:\\Users\\2369634\\Documents\\GitHub\\Programming\\Programming\\Fibonacci";
-        String directoryName = "Fibonacci";
+    private static DatabaseHandler handler;
+    private static void printTable(String tableName) {
+        String qu = "SELECT * FROM " + tableName;
+        ResultSet resultSet = handler.execQuery(qu);
+        try{
+            while (resultSet.next()){
+                String path = resultSet.getString("path");
+                String name = resultSet.getString("name");
+                String extension = resultSet.getString("extension");
+                String size = resultSet.getString("size");
+                System.out.println(path + " " + name + " " + extension + " " + size);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addTable(String folderPath) {
+        String directoryName = parseName(folderPath);
         handler.createTable(directoryName);
         File folder = new File(folderPath);
         Collection<File> files = FileUtils.listFiles(folder, null, true);
+        String st2 = "INSERT INTO FOLDERS VALUES ('"+ directoryName + "')";
+        handler.execAction(st2);
         for (File file : files) {
             System.out.println(file);
+            Path path = Paths.get(file.getAbsolutePath());
+            String size = null;
+            try {
+                size = Files.size(path) + "bytes";
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String st = "INSERT INTO "+ directoryName + " VALUES ("+
+                    "'" + file.getName() + "',"+
+                    "'" + file.getAbsolutePath() + "',"+
+                    "'" + FilenameUtils.getExtension(file.getName()) + "',"+
+                    "'" + size + "'"+ ")";
+            handler.execAction(st);
         }
+    }
+
+    private static String parseName(String path) {
+        return new File(path).getName();
+    }
+
+    public static void main(String[] args) throws IOException {
+        handler=new DatabaseHandler();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose folder path:");
+        String path = scanner.nextLine();
+        File file = new File(path);
+        if(!file.exists()){
+            System.out.println("File does not exist");
+        }
+        String tableName = parseName(path);
+        addTable(path);
+        printTable(tableName);
+        
     }
 }
--- Programming/database2/src/main/java/database/DatabaseHandler.java
+++ Programming/database2/src/main/java/database/DatabaseHandler.java
@@ -6,9 +6,30 @@ public class DatabaseHandler {
     private static final String DB_url = "jdbc:derby:database/forum;create=true";
     private static Connection conn = null;
     private static Statement stmt = null;
+    public ResultSet execQuery;
 
     public DatabaseHandler(){
         createConnection();
+        createFoldersTable();
+    }
+
+    private void createFoldersTable() {
+        String TABLE_NAME = "FOLDERS";
+        try {
+            stmt = conn.createStatement();
+            DatabaseMetaData dmn = conn.getMetaData();
+            ResultSet tables = dmn.getTables(null, null, TABLE_NAME, null);
+            if (tables.next()){
+                System.out.println("Table " + TABLE_NAME + " exists");
+            }else {
+                String statement = "CREATE TABLE " + TABLE_NAME + "("
+                        + "name varchar(200))";
+                System.out.println(statement);
+                stmt.execute(statement);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void createTable(String folderName) {
@@ -50,6 +71,18 @@ public class DatabaseHandler {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public ResultSet execQuery(String qu) {
+        ResultSet resultSet;
+        try{
+            stmt=conn.createStatement();
+            resultSet = stmt.executeQuery(qu);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return resultSet;
     }
 }
 
